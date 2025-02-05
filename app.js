737afb1ab1fe2802855c60d931661e5075963204
Mandi <mandibeauchaine1@gmail.com>
2025-02-05 14:22:39 -0700

    combine swagger and other routes on my app.js

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 const app = express();
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-app
-.use(cors())
-.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app
     .use(cors())
@@ -19,6 +17,7 @@ app
         res.setHeader('Access-Control-Allow-Origin', '*');
         next()
     })
+    .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
     .use('/', require('./routes/'));
 
 mongodb.initDb((err) => {
